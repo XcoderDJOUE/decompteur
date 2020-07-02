849fe5335280a46bc794965d312c7e6d89a86f40
Djouekan joel armel <djouejoel@gmail.com>
2020-07-02 14:27:29 +0000

    Update counter.js

--- counter.js
+++ counter.js
@@ -17,8 +17,11 @@ function dayDiff(d1, d2)
   d2 = d2mils / 86400000;
   var dso = new Date().getTime();
   var d3 = dso / 86400000;
+  if(d2 > d1 && (d3 >= d1 && d3 > d2)){
+       return {success : {days: -1 ,month: -1 ,years: -1 ,hours: -1 ,minute: -1 ,seconde: -1}} ;
+  }
     if(d2 > d1 && (d3 >= d1 && d3 <= d2)){
-              var nJ = decompte(d1nj, d2nj);
+      var nJ = decompte(d1nj, d2nj);
       var ret = nbYear(new Number(nJ.jours).toFixed(0));  
       if( ret ){
           ret.hours = nJ.heure;
@@ -34,7 +37,21 @@ function dayDiff(d1, d2)
 
 }
 }
-
+ 
+ function luncher(todaypp, oldDate){
+   setInterval(()=>{
+     
+      var retour  = dayDiff(todaypp, oldDate);
+      if(!retour.success){
+          $('#decompt').html(retour.hours+' : '+retour.minute+' : '+retour.seconde );
+      }else{
+         // return  {days: 0 ,month: 0 ,years: 0 ,hours: 0 ,minute: 0 ,seconde: 0} ;
+          $('#red').show(1000);
+        // windows.location.href="#";
+      }
+      
+      }, 1000);
+ }
 function decompte(date_deb, cible) {
 var mms_jour = 24 * 60 * 60 * 1000;
 var mms_heure = 60 * 60 * 1000;
@@ -55,5 +72,5 @@ var diff_secondes = Math.floor(diff_mms / mms_seconde);
 return {'jours': diff_jours, 'heure': diff_heures, 'minute': diff_minutes, 'seconde': diff_secondes};
 }
 var todaypp = new Date('07/01/2020 12:30:50');
-var oldDate = new Date('07/02/2021 23:00:50');
-console.log(dayDiff(todaypp, oldDate));
+var oldDate = new Date('07/02/2020 11:00:50');
+console.log(luncher(todaypp, oldDate));
